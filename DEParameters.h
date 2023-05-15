6f233c1d176a3a646fa9bc56f8a3d5d07b73c508
BradRanger <115566819+BradRanger@users.noreply.github.com>
2023-05-15 16:21:33 -0400

    Added class names to DEParameters

--- DEParameters.h
+++ DEParameters.h
@@ -1,1 +1,21 @@
 #pragma once
+#ifndef DEPARAMATERS_H
+#define DEPARAMATERS_H
+
+
+class DEParameters {
+
+	size_t mPopulationSize;
+	double mF;
+	double mCR;
+	double mMaxGenerationCount;
+	de_objective_function_t_mObjFunc();
+	de_fitness_function_t_mFitnessFunc();
+
+
+
+};
+
+
+
+#endif
