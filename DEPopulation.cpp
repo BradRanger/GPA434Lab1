65b7156cf85fcc07e85dffe10543958f4ff2016c
icejuic3 <133609558+icejuic3@users.noreply.github.com>
2023-05-19 18:27:24 -0400

    correction espace

--- DEPopulation.cpp
+++ DEPopulation.cpp
@@ -9,10 +9,10 @@ size_t DEPopulation::size() const
 	return size_t();
 }
 
-void DEPopulation::setup(size_t populationSize, DESolutionBounds const& solutionBounds)
+void DEPopulation::setup(size_t populationSize, DESolutionBounds const & solutionBounds)
 {
 }
 
-void DEPopulation::randomize(DESolutionBounds const& solutionBounds)
+void DEPopulation::randomize(DESolutionBounds const & solutionBounds)
 {
 }
