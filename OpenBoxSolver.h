b7bdd3326835887ebf4268b98229eae73176f9b1
BradRanger <115566819+BradRanger@users.noreply.github.com>
2023-06-02 13:35:20 -0400

    cours 6

--- OpenBoxSolver.h
+++ OpenBoxSolver.h
@@ -1,1 +1,32 @@
 #pragma once
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// classe open box problem
+// 
+// width
+// height
+// -----------------------------------------------------------
+// 
+// 
+// 
+// 
+// 
+// 
+// exterieur de classe
+// double fonction objective(DESolution const &)
+// calcule si c bon ou pas
+//()operateur dexecutiion
+// double operator()(DESolution const &)
+// functor est un objet executable. un objet dont les parentheses sont surchargees
+// une fonction conventionnelle ne permet pas de gerer des donnees habilement 
+//
+//
+//
