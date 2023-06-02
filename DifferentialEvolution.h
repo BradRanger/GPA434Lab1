ae8c34d79aacd5e6799a64bed17ef37f7b6f07da
icejuic3 <133609558+icejuic3@users.noreply.github.com>
2023-06-02 19:35:19 -0400

    .h fait

--- DifferentialEvolution.h
+++ DifferentialEvolution.h
@@ -2,24 +2,48 @@
 #ifndef DIFFERENTIAL_EVOLUTION_H
 #define DIFFERENTIAL_EVOLUTION_H
 #include "interval.h"
+#include "DEStatistics.h"
+#include "ThreeSamplesWithOneExclusion.h"
+
+
 #include <vector>
 #include "DEParameters.h"
 #include "DEPopulation.h"
 
 class DifferentialEvolution{
 
-	DifferentialEvolution();
-	~DifferentialEvolution();
-	bool isReady() const;
-	void setup(DEParameters const& paremeters);
-	void reset();
-	void evolveOne();
-	void evolve();
+	DESolutionBounds mParameters;
+	DEPopulation mPopulation;
+	DEPopulation mMutant;
+	DEPopulation mTrial;
+	size_t mCurrentGeneration;
+	DEStatistics mStatistics;					//a faire
+	ThreeSamplesWithOneExclusion mSamplingTool; //a faire
+
+
+	
 	void processFitness(DEPopulation& population);
 	void ProcessMutation();
 	void ProcessCrossover();
 	void ProcessSelection();
 	void ProcessStatistics();
+
+
+
+public:
+
+
+	DifferentialEvolution();
+	~DifferentialEvolution();
+
+
+	bool isReady() const;
+	void setup(DEParameters const& paremeters);
+	void reset();
+	bool evolveOne();
+	bool evolve();
+
+
 };
 
 #endif
