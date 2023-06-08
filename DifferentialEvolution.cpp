cfe503592401bb5d403cb038e8f3ad4d2c494e15
icejuic3 <133609558+icejuic3@users.noreply.github.com>
2023-06-08 04:21:34 -0400

    PLUS DE MODI

--- DifferentialEvolution.cpp
+++ DifferentialEvolution.cpp
@@ -9,24 +9,22 @@ DifferentialEvolution::DifferentialEvolution()
 ,mStatistics{}
 ,mSamplingTool{}
 {
-
 }
 
 
 DifferentialEvolution::~DifferentialEvolution() 
 {
-
 }
 
 bool DifferentialEvolution::isReady() const
 {
 
-	//Verifier que les parametres necessaires ont ete definis
+	//Verifie que les parametres necessaires ont ete definis
 	if (mParameters.getPopulationSize() == 0 || mParameters.getMaxGenerationCount() == 0 || mParameters.getF() <= 0 || mParameters.getCR() < 0 || mParameters.getCR() > 1) {
 		return false;
 	}
 
-	// Vérifiez que mPopulation, mMutant et mTrial ont la bonne taille
+	// Vérifie que mPopulation, mMutant et mTrial ont la bonne taille
 	if (mPopulation.size() != mParameters.getPopulationSize() || mMutant.size() != mParameters.getPopulationSize() || mTrial.size() != mParameters.getPopulationSize()) {
 		return false;
 	}
@@ -49,26 +47,38 @@ bool DifferentialEvolution::isReady() const
 
 void DifferentialEvolution::setup(DEParameters const& parameters)
 {
-	
 	mSamplingTool.prepare(mParameters.getPopulationSize());
-
 	mPopulation.setup(mParameters.getPopulationSize(), mParameters.getSolutionBounds());	//corriger mParameters, c'est un mSolutionBounds 
 	mTrial.setup(mParameters.getPopulationSize(), mParameters.getSolutionBounds());
 	mMutant.setup(mParameters.getPopulationSize(), mParameters.getSolutionBounds());
-	
 }  
 
 void DifferentialEvolution::reset()
 {
-	mPopulation.setup(mPopulation.size(), mParameters.getSolutionBounds() );
+	// Réinitialisez la population de solutions
+	mPopulation.setup(mParameters.getPopulationSize(), mParameters.getSolutionBounds());
 	mPopulation.randomize(mParameters.getSolutionBounds());
 
+	// Réinitialisez la population de solutions
+	mMutant.setup(mParameters.getPopulationSize(), mParameters.getSolutionBounds());
+	mMutant.randomize(mParameters.getSolutionBounds());
+
+	// Réinitialisez la population de solutions
+	mTrial.setup(mParameters.getPopulationSize(), mParameters.getSolutionBounds());
+	mTrial.randomize(mParameters.getSolutionBounds());
 
 
+	// Réinitialisez la génération actuelle
+	mCurrentGeneration = 0;
+
+	// Réinitialisez les statistiques
+	mStatistics.reset();
 
 
+	// Réinitialisez l'outil d'échantillonnage
+	mSamplingTool = ThreeSamplesWithOneExclusion();
 
-
+	/*A TESTER POUR VERIFIER QUE TOUT SE REINITILAISE CORRECTEMENT*/
 }
 
 bool DifferentialEvolution::evolveOne()
